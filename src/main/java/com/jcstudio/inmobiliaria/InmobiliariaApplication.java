1d53416c4bf205ba8f5dfe7d3fb58522c38f8c86
Javier Ruiz <javier.rp.dev@gmail.com>
2026-05-23 15:01:07 -0600

    Initial commit

---
+++ src/main/java/com/jcstudio/inmobiliaria/InmobiliariaApplication.java
@@ -0,0 +1,13 @@
+package com.jcstudio.inmobiliaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmobiliariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InmobiliariaApplication.class, args);
+	}
+
+}
