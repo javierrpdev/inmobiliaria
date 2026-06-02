7eac2caf69d27945815d2bec7678c4aee4b56844
Javier Ruiz <javier.rp.dev@gmail.com>
2026-06-01 18:35:18 -0600

    se crea el front de login

--- src/main/java/com/jcstudio/inmobiliaria/controller/HomeController.java
+++ src/main/java/com/jcstudio/inmobiliaria/controller/HomeController.java
@@ -3,11 +3,11 @@ package com.jcstudio.inmobiliaria.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class HomeController {
 
-    @GetMapping("/") // Esto le dice a Java que responda cuando entres a localhost:8080
+    @GetMapping("/")
     public String inicio() {
-        // Le dice a Thymeleaf que busque en templates/publico/index.html
-        return "public/index";
+        return "/login"; // Buscará en templates/public/index.html
     }
 }
