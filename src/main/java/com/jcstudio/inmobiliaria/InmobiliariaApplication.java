3d6304caecd0b59ce30bd0ca3d57a40c4050db9a
rodes115 <rodesreyes115@gmail.com>
2026-06-02 17:00:37 -0600

    Realizado las variables del entorno para la BD

--- src/main/java/com/jcstudio/inmobiliaria/InmobiliariaApplication.java
+++ src/main/java/com/jcstudio/inmobiliaria/InmobiliariaApplication.java
@@ -3,12 +3,12 @@ package com.jcstudio.inmobiliaria;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {
-		org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
-		org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
-})
+//@SpringBootApplication(exclude = {
+//		org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+//		org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
+//})
 
-//@SpringBootApplication
+@SpringBootApplication
 public class InmobiliariaApplication {
 
 	public static void main(String[] args) {
