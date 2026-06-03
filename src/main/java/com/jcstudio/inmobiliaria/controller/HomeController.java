38bc8807cbd158d57d6326871fbabc329a893349
Javier Ruiz <javier.rp.dev@gmail.com>
2026-06-02 20:31:38 -0600

    "Se agrega el admin.html"

--- src/main/java/com/jcstudio/inmobiliaria/controller/HomeController.java
+++ src/main/java/com/jcstudio/inmobiliaria/controller/HomeController.java
@@ -1,12 +1,15 @@
 package com.jcstudio.inmobiliaria.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+
 
 @Controller
 public class HomeController {
-
-
 
     @GetMapping("/")
     public String inicio() {
@@ -17,6 +20,10 @@ public class HomeController {
     public String login() {
         return "login"; // O "public/login" si él metió el login.html a la carpeta public
     }
+
+
+
+
 
 
 }
